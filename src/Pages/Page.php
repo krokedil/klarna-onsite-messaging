8c87a0810b263973735dde2d9d2a0b91ac9520ce
MichaelBengtsson <michael@krokedil.se>
2024-06-20 08:47:59 +0200

    Change logic for enabled check and where to get the data_client_id setting

--- src/Pages/Page.php
+++ src/Pages/Page.php
@@ -87,6 +87,7 @@ abstract class Page {
 		$this->settings   = $settings;
 
 		$this->update( $settings );
+		$this->enabled = ! empty( $this->placement_id );
 	}
 
 	/**
@@ -97,15 +98,21 @@ abstract class Page {
 	 */
 	protected function update( $settings ) {
 		foreach ( $this->properties as $key => $value ) {
+			// Maybe get the client id from the KP settings.
+			if ( 'data_client_id' === $key && function_exists( 'kp_get_client_id' ) ) {
+				$this->{$key} = kp_get_client_id();
+				continue;
+			}
+
 			if ( is_array( $value ) ) {
 				$this->{$key} = $settings->get( $value['setting'], $value['default'] );
 			} else {
 				$this->{$key} = $settings->get( $value );
 			}
+		}
 
-			if ( in_array( $this->{$key}, array( 'yes', 'no' ) ) ) {
-				$this->{$key} = wc_string_to_bool( $this->{$key} );
-			}
+		if ( in_array( $this->{$key}, array( 'yes', 'no' ) ) ) {
+			$this->{$key} = wc_string_to_bool( $this->{$key} );
 		}
 	}
 
