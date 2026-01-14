6f15262f1389706bb0a479b769696b801f0dd14b
anyakro <anya@krokedil.se>
2026-01-14 07:44:42 +0100

    Comment out locale for now

--- src/Blocks/CartBlockIntegration.php
+++ src/Blocks/CartBlockIntegration.php
@@ -54,7 +54,7 @@ class CartBlockIntegration implements IntegrationInterface {
 		$key             = apply_filters( 'kosm_cart_block_key', '' );
 		$theme           = apply_filters( 'kosm_cart_block_theme', 'default' );
 		$purchase_amount = apply_filters( 'kosm_cart_block_purchase_amount', '' );
-		$locale          = Utility::get_locale_from_currency();
+		$locale          = ''; // Utility::get_locale_from_currency();
 
 		return array(
 			'key'             => $key,
