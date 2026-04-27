14012d97979ef6572f146ead7a4c7f499988c221
Montazar <github.mdtk4@passmail.com>
2026-04-27 11:31:20 +0200

    Retrieves ExperimentalOrderMeta from global wc

--- src/assets/js/osm-cart-block-integration.js
+++ src/assets/js/osm-cart-block-integration.js
@@ -1,36 +1,46 @@
 const { registerPlugin: RegisterPluginOSM } = wp.plugins
-const { useSelect } = wp.data
 
-const ElementOSM = ({ klarnaKey, locale, theme, purchaseAmount }) => {
-    const totalPrice = useSelect((select) => select("wc/store/cart").getCartTotals()?.total_price)
+const ElementOSM = ({ klarnaKey, locale, theme, purchaseAmount, cart }) => {
+    const [showPlacement, setShowPlacement] = React.useState(true)
 
     React.useEffect(() => {
-        if (window.klarna_onsite_messaging && totalPrice !== undefined && totalPrice !== null) {
-            window.klarna_onsite_messaging.update_total_price(totalPrice)
+        if (window.klarna_onsite_messaging) {
+            window.klarna_onsite_messaging.update_total_price(cart.cartTotals.total_price)
         }
-    }, [totalPrice])
+    }, [cart.cartTotals.total_price])
 
-    return React.createElement("klarna-placement", {
-        className: "klarna-onsite-messaging",
-        "data-preloaded": "true",
-        "data-key": klarnaKey,
-        "data-locale": locale,
-        "data-theme": theme,
-        "data-purchase-amount": purchaseAmount,
-    })
+    return showPlacement
+        ? React.createElement("klarna-placement", {
+              className: "klarna-onsite-messaging",
+              "data-preloaded": "true",
+              "data-key": klarnaKey,
+              "data-locale": locale,
+              "data-theme": theme,
+              "data-purchase-amount": purchaseAmount,
+          })
+        : null
 }
 
 const renderOSM = () => {
+    const { ExperimentalOrderMeta } = window.wc?.blocksCheckout || {};
+    if (!ExperimentalOrderMeta) {
+        console.warn("[Klarna OSM] ExperimentalOrderMeta not found in window.wc.blocksCheckout")
+    }
+
     const osmData = window.wc?.wcSettings?.getSetting("osm-cart-block-integration_data", {}) || {}
-    return React.createElement(ElementOSM, {
-        klarnaKey: osmData.key || "",
-        locale: osmData.locale || "",
-        theme: osmData.theme || "",
-        purchaseAmount: osmData.purchase_amount || "",
-    })
+    return React.createElement(
+        ExperimentalOrderMeta,
+        null,
+        React.createElement(ElementOSM, {
+            klarnaKey: osmData.key || "",
+            locale: osmData.locale || "",
+            theme: osmData.theme || "",
+            purchaseAmount: osmData.purchase_amount || "",
+        }),
+    )
 }
 
 RegisterPluginOSM("osm-cart-block-integration", {
     render: renderOSM,
-    scope: "woocommerce-cart",
+    scope: "woocommerce-checkout",
 })
